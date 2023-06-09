9800d5c897d414946aaa84045b65b040caaba8f6
Aakash Kaushik <kaushikaakash527@gmail.com>
2023-06-09 09:17:04 +0530

    Third Commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-
+import colors from "colors";
 const connectDB = async () => {
-    try {
-        const conet = await mongoose.connect(process.env.MONGO_URL)
-        console.log(`Connect to  Mongodb ${conet.connection.host}`)
-    } catch (error) {
-        console.log(`Error in mongodb ${error}`)
-    }
-}
-export default connectDB
+  try {
+    const conet = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conet.connection.host}`.bgMagenta
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed);
+  }
+};
+
+export default connectDB;
